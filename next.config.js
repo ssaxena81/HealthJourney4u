3f5ca73d9ac1041f1bbc501a23ae3c08b17b9d8f
Sharad Saxena <arthaventures81@gmail.com>
2025-06-21 17:36:45 +0000

    npm build throws following errors:  /home/user/studio/node_modules/next/

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    // ignoreBuildErrors: true, // Temporarily removed to surface potential issues
+  },
+  eslint: {
+    // ignoreDuringBuilds: true, // Temporarily removed to surface potential issues
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      // Add other image source hostnames here if needed
+    ],
+  },
+  serverExternalPackages: ['@grpc/grpc-js'], // Keep gRPC external
+};
+
+module.exports = nextConfig;
